d69e2648c67f28c1d15e162b8aa39008597ab408
SergonM <sa.gonzalezm123@uniandes.edu.co>
2023-12-23 23:09:22 -0500

    add: entry page, page not fund

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from markdown2 import Markdown
 from django.shortcuts import render
+from . import util
 
 from . import util
 
@@ -7,4 +9,14 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def wiki_entry(request, entry_title):
+    entry = util.get_entry(entry_title)
+
+    if (entry == None):
+        return render(request, "encyclopedia/not_found.html")
+
+    markdowner = Markdown()
+    entry_html = markdowner.convert(entry)
+    return render(request, "encyclopedia/wiki_entry.html", {"name": entry_title, "entry_html": entry_html})
 
